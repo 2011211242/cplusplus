032b8820d96ba49642be7c0b45c59d025406176c
2011211242 <2990936748@qq.com>
2017-09-08 17:46:38 +0800

    dadf

--- thread/main.cpp
+++ thread/main.cpp
@@ -3,6 +3,7 @@
 #include <unistd.h>
 #include <thread>
 #include <mutex>
+#include <algorithm>
 #include <condition_variable>
 using namespace std;
 
@@ -18,9 +19,11 @@ condition_variable not_full;
 condition_variable not_empty;
 
 bool fin = false;
-int count = 100;
+int _count = 0;
+int max_count = 0;
 
 queue<int> Q;
+
 
 void task(int th_id){
     bool ready_to_exit = false;
@@ -31,6 +34,8 @@ void task(int th_id){
             //unique_lock<mutex> lock(producing_mtx);
             //printf("thread_id=%d producing_mtx\n", th_id);
             unique_lock<mutex> lock1(mtx);
+            _count++;
+            max_count = max(_count, max_count);
             //printf("thread_id=%d mtx\n", th_id);
 
             if(Q.size() == 0 && producing == 0){
@@ -48,24 +53,29 @@ void task(int th_id){
 
             //lock1.unlock();
             //lock.unlock();
+            _count--;
             if(ready_to_exit) break;
         }
 
         for(int i = 0; i < 1000000; i++);
-        printf("thread_id=%d, idx = %d\n", th_id, idx);
+        printf("thread_id=%d, idx = %d, max_count = %d\n", th_id, idx, max_count);
 
         {
             unique_lock<mutex> lock2(mtx);
+            _count++;
+            max_count = max(_count, max_count);
             producing++;
             //unique_lock<mutex> lock3(mtx);
 
-            if(idx * 2 + 2 < 100000)
+            if(idx * 2 + 2 < 1000)
             {
                 Q.push(idx * 2);
                 Q.push(idx * 2 + 1);
             }
 
             producing --;
+            _count--;
+
             not_empty.notify_all();
             //lock3.unlock();
             //lock2.unlock();
@@ -86,10 +96,10 @@ int main(){
 
     //printf("locked\n");
 
-    int thread_num = 4;
+    int thread_num = 8;
     for(int i = 0; i < thread_num; i++){
         threads.push_back(thread(task, i));
-        sleep(1);
+        usleep(1000);
     }
 
     for(int i = 0; i < thread_num; i++){
